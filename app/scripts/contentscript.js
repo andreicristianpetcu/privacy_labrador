c9d6d82a787040d962867e4de36ed2309d076a0d
Andrei Cristian Petcu <andrei@ceata.org>
2018-04-25 01:08:17 +0300

    added rudimentary qwant support

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,19 +1,45 @@
-function injectGoogleLink(locationSearch) {
+function createGoogleLink(searchTerms) {
+    var googleLink = document.createElement('a');
+    googleLink.textContent = "Google";
+    googleLink.href = "https://www.google.com/search?q=" + searchTerms;
+    return googleLink;
+}
+
+function decorageDuckDuckGoLink(googleLink){
+    googleLink.className = 'zcm__link  js-zci-link  js-zci-link--images';
+
+    var googleLinkItem = document.createElement('li');
+    googleLinkItem.className = 'zcm__item';
+    googleLinkItem.appendChild(googleLink);
+    var resultItems = document.getElementById('duckbar_static');
+    resultItems.insertBefore(googleLinkItem, resultItems.childNodes[0]);
+}
+
+function decorateQwantLink(googleLink){
+    googleLink.className = 'sidebar__item__link';
+
+    var googleLinkItem = document.createElement('li');
+    googleLinkItem.className = 'sidebar__item sidebar__item--google';
+    googleLinkItem.appendChild(googleLink);
+    var resultItems = document.getElementsByClassName('sidebar__content')[0];
+    resultItems.insertBefore(googleLinkItem, resultItems.childNodes[0]);
+}
+
+function injectGoogleLink(hostName, locationSearch) {
     var queryParam = new URLSearchParams(locationSearch).get('q');
 
     if (queryParam) {
-        var googleLinkItem = document.createElement('li');
-        googleLinkItem.className = 'zcm__item';
+        var googleLink = createGoogleLink(queryParam);
 
-        var googleLinkAnchor = document.createElement('a');
-        googleLinkAnchor.textContent = "Google";
-        googleLinkAnchor.href = "https://www.google.com/search?q=" + queryParam;
-        googleLinkAnchor.className = 'zcm__link  js-zci-link  js-zci-link--images';
-        googleLinkItem.appendChild(googleLinkAnchor);
+        if(hostName.indexOf('duckduckgo.com') > -1){
+            decorageDuckDuckGoLink(googleLink);
+        } else if(hostName.indexOf('qwant.com') > -1){
+            decorateQwantLink(googleLink);
+        }
 
-        var resultItems = document.getElementById('duckbar_static');
-        resultItems.insertBefore(googleLinkItem, resultItems.childNodes[0]);
     }
 }
 
-injectGoogleLink(window.location.search);
+setTimeout(function(){
+    injectGoogleLink(window.location.hostname, window.location.search);
+}, 500);
--- test/ContentScriptSpec.js
+++ test/ContentScriptSpec.js
@@ -6,11 +6,22 @@
         it('should work for DuckDuckGo', function () {
             document.body.innerHTML = window.__html__['test/data/duck_duck_go_result.html'];
 
-            injectGoogleLink('?q=karma+load+file&t=ffab&ia=qa');
+            injectGoogleLink('duckduckgo.com', '?q=gnu+project&t=ffab&ia=qa');
 
             var googleAnchorInDuckDuckGoPage = document.querySelectorAll("ul#duckbar_static li a")[0];
             expect(googleAnchorInDuckDuckGoPage.text).toBe('Google');
-            expect(googleAnchorInDuckDuckGoPage.href).toBe('https://www.google.com/search?q=karma%20load%20file');
+            expect(googleAnchorInDuckDuckGoPage.href).toBe('https://www.google.com/search?q=gnu%20project');
+        });
+
+
+        it('should work for Qwant desktop', function () {
+            document.body.innerHTML = window.__html__['test/data/qwant_desktop.html'];
+
+            injectGoogleLink('www.qwant.com', '?q=gnu+project&t=web');
+
+            var googleAnchorInDuckDuckGoPage = document.querySelectorAll("li.sidebar__item--google a")[0];
+            expect(googleAnchorInDuckDuckGoPage.text).toBe('Google');
+            expect(googleAnchorInDuckDuckGoPage.href).toBe('https://www.google.com/search?q=gnu%20project');
         });
 
     });
